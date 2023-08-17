c2436f884fe27263edf8266442d3b639438e2550
mah <mahsa.79mirmiran@gmail.com>
2023-08-18 01:45:21 +0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+
+
+<template>
+  <div class="home flex flex-col justify-center items-center align-middle">
+    <div class="">
+      <button class="w-auto bg-zinc-700 hover:bg-cyan-900 bg-opacity-80 mx-2 p-3 py-1.5 rounded btn cursor-pointer">-</button>
+      <button class="w-auto bg-zinc-700 hover:bg-cyan-900 bg-opacity-80 mx-2 p-3 py-1.5 rounded counter">0</button>
+      <button class="w-auto bg-zinc-700 hover:bg-cyan-900 bg-opacity-80 mx-2 p-3 py-1.5 rounded btn">+</button>
+    </div>
+  </div>
+</template>
